620c4c01aa70c6793b1cdabd40441af03534e49d
ASYYves <yvesbullet.carranza29@gmail.com>
2025-06-27 19:38:01 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,36 @@
-﻿using IDSystemBusinessLogic;
-using IDSystemData;
-using IDSystemData.IDSystemData;
-using IDSystemGUI;
-using System;
-using System.Threading;
 
-namespace IDAttendance
+namespace IDSystemWeb
 {
-
-    class Program
+    public class Program
     {
-
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
 
-            //db, json, or txt
-            const string modeOfStorage = "db";
-            var studentStorageType = modeOfStorage == "db" ? "db": modeOfStorage == "json" ? "DataStudents.json" : "StudentsAttendance.txt";
-            var attendanceStorageType = modeOfStorage == "db" ? "db" : modeOfStorage == "json" ? "AttendanceStudents.json" : "StudentsAttendance.txt";
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
+            var app = builder.Build();
 
-            //call type of storage to use
-            var toStoreStudent = storageFormat.selectStudentStorage(modeOfStorage, studentStorageType);
-            var toStoreAttendance = storageFormat.selectAttendanceStorage(modeOfStorage, attendanceStorageType);
-
-
-            //start business logic
-            Checking.setStorageLocation(toStoreStudent, toStoreAttendance);
-
-
-            //loop until exit
-            while (true)
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
             {
-
-
-                Console.WriteLine("Welcome PUPIan!\n");
-                Checking.AutoClockOutAll(TimeSpan.FromMinutes(10));
-
-
-                //return ex, admin or valid ID
-                var id = Displays.StudentId;
-                if (id.Equals("ex", StringComparison.OrdinalIgnoreCase))
-                {
-
-
-                    Console.WriteLine("Loop ends");
-                    break;
-
-
-                }
-
-
-                //show student info
-                Displays.displayStudentInfo();
-                Console.WriteLine("\n\n\n");
-
-
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
+            app.UseHttpsRedirection();
 
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-
-
     }
-
-    
 }
